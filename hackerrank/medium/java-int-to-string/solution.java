f36094939e803e981dbd09d40395314efa216dde
GunjaKumari5535 <gk8235541649@gmail.com>
2026-08-24 23:09:47 +0530

    ✅ Java Int to String | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

--- hackerrank/medium/java-int-to-string/solution.java
+++ hackerrank/medium/java-int-to-string/solution.java
@@ -1,31 +1,4 @@
-import java.io.*;
-import java.util.*;
 
-public class Solution {
+String s = String.valueOf(n);
+   //Write your code here
 
-    
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        
-        static int  B;
-        static int H;
-        static boolean flag = false;
-        
-        static {
-            Scanner sc = new Scanner(System.in);
-                 B = sc.nextInt();
-                 H = sc.nextInt();
-                 sc.close();
-                 if(B>0 && H>0){
-                    flag = true;
-                  }  else {
-                        System.out.println("java.lang.Exception: Breadth and height must be positive");
-                    }
-                 }
-                 public static void main(String[] args){
-                    if(flag){
-                        int area = B*H;
-                        System.out.print(area);
-                    }
-                 }
-            }
-       
